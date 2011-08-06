b6da08202394ae5e7384eca5d21d732b324cad55
Jim Frantzen <jim.frantzen@gmail.com>
2011-08-06 17:58:02 -0500

    first network test

---
+++ src/Slime.Console/tests/NetworkTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+
+namespace Slime.tests {
+    [TestFixture]
+    public class NetworkTests {
+        [Test]
+        public void GetNetworkResources_Returns_InitialGETRequestURL() {
+            var slimer = new Slimer("http://jimchely.com/", 1024, 768);
+            string[] resources = slimer.GetNetworkResources();
+            var item = resources.Contains("http://jimchely.com/");
+            Assert.IsTrue(item);
+        }
+    }
+}
